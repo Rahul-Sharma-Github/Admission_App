780eb9e8ea0d5e3a5ce6dab6e702a73a6c4a7ca2
Rahul <rahulsharma3258@gmail.com>
2022-10-27 19:14:19 +0530

    SubmittedActivity Page Added. And Title,SubTitle, (Animated Done Icon) Added.

---
+++ app/src/main/java/com/example/imageslider/SubmittedActivity.java
@@ -0,0 +1,14 @@
+package com.example.imageslider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubmittedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submitted);
+    }
+}
