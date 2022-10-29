6e4952cae4bad918ca014d075bdb545577b7a687
Rahul <rahulsharma3258@gmail.com>
2022-10-29 18:11:40 +0530

    Again upgraded build.gradle file to new version.

--- app/src/main/java/com/example/imageslider/FormScrollingActivity.java
+++ app/src/main/java/com/example/imageslider/FormScrollingActivity.java
@@ -1,19 +1,15 @@
 package com.example.imageslider;
 
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Bundle;
-
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-
 import com.example.imageslider.databinding.ActivityFormScrollingBinding;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
 
 public class FormScrollingActivity extends AppCompatActivity {
 
