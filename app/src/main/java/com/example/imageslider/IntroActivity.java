c994fd7dd0c0eb883af9ed3dd021c517bf7ceb95
Rahul <rahulsharma3258@gmail.com>
2022-10-30 19:00:33 +0530

    Added Handler class instance to make screen wait for a specific time and then Navigate to the Home Screen (MainActivity Page).

--- app/src/main/java/com/example/imageslider/IntroActivity.java
+++ app/src/main/java/com/example/imageslider/IntroActivity.java
@@ -1,14 +1,33 @@
 package com.example.imageslider;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class IntroActivity extends AppCompatActivity {
 
+    //Declaring Handler type handler
+    public Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        //making an instance of Handler Class
+        handler=new Handler();
+        //Adding postDelayed() method to wait for a specific time
+        handler.postDelayed(new Runnable() {
+            @Override
+            //after a specific time the it Navigate to the MainActivity Page (Home Page)
+            public void run() {
+                Intent i=new Intent(IntroActivity.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },5000); // 5 Seconds Delay
+
     }
 }
