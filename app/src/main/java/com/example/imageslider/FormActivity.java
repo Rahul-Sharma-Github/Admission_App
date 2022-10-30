1492c111fdc61f6c4be7fad463f6433768e6d7b9
Rahul <rahulsharma3258@gmail.com>
2022-10-30 15:16:17 +0530

    Added Navigation to the SubmittedActivity Page on click of Submit Form button.

--- app/src/main/java/com/example/imageslider/FormActivity.java
+++ app/src/main/java/com/example/imageslider/FormActivity.java
@@ -1,14 +1,34 @@
 package com.example.imageslider;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FormActivity extends AppCompatActivity {
 
+    Button btnSubmit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        //Binding the Submit Form Button
+        btnSubmit=(Button) findViewById(R.id.btnSubmitForm);
+
+        //Navigating page to SubmittedActivity Page
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(FormActivity.this,SubmittedActivity.class);
+                startActivity(in);
+            }
+        });
+
+
+
     }
 }
